3495c778ddf81aefa8f416c6036406008b2b1a2f
Rahul Duggal <duggalr42@gmail.com>
2022-06-16 22:37:31 -0400

    changes

--- rooms/views.py
+++ rooms/views.py
@@ -74,7 +74,7 @@ def profile(request):
       'last_name': user_last_name
     }
     # TODO: need profile data
-    return render(request, 'profile_new_two.html', {'user_info': user_info})
+    return render(request, 'profile_new_three.html', {'user_info': user_info})
 
   else:  # TODO: redirect to landing
     print(request)
